24d8943fd7b1bd380f6ddb237fda738b4a99e3de
Dmitriy Rotatyy <flamine@list.ru>
2017-02-22 22:43:17 +0300

    add  "not found" and "error" pages

--- server.rb
+++ server.rb
@@ -1,5 +1,14 @@
-require 'sinatra/base'
+require 'sinatra'
+configure { set :server, :puma } # use puma like default web server
 get '/' do
   code = "<%= Time.now %>"
   erb code
+end
+
+not_found do
+  'This is nowhere to be found.'
+end
+
+error do
+  'Sorry there was a nasty error - ' + env['sinatra.error'].message
 end
