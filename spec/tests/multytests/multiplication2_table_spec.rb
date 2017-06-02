809d2ced5c71fb8bf5b0cae745ccb42437f6241d
flaminestone <flamine@list.ru>
2017-06-02 17:07:45 +0300

    add  multiple test spec and api for result adding

--- spec/tests/multytests/multiplication2_table_spec.rb
+++ spec/tests/multytests/multiplication2_table_spec.rb
@@ -1,28 +1,27 @@
 require 'net/http'
+require 'json'
+require_relative '../../data/static_data'
 class Palladium
   def initialize(*args)
+    @http = Net::HTTP.new(args.first[:auth][:host], args.first[:auth][:port])
     @product = args.first[:product]
     @plan = args.first[:plan]
     @run = args.first[:run]
-    @email = args.first[:auth][:email]
-    @password = args.first[:auth][:password]
-    @http = Net::HTTP.new(args.first[:auth][:host], args.first[:auth][:port])
+    @run_id = nil
   end
 
   def set_result(example)
     result = get_result(example)
-    request = Net::HTTP::Post.new('/result_new', 'Content-Type' => 'application/json')
-    request.set_form_data({
-                              "product_data[name]":@product,
-                              "plan_data[name]": @plan,
-                              "run_data[name]": @run,
-                              "result_set_data[name]": example.metadata[:description],
-                              "result_data[message]": "123123123123",
-                              "result_data[status]": "#{result.first}",
-                              "user_data[email]": @email,
-                              "user_data[password]": @password
-                          })
-    @http.request(request)
+    request = Net::HTTP::Post.new('/api/result_new', 'Authorization' => StaticData::TOKEN)
+    params = {"plan_data[product_name]": @product,
+              "plan_data[name]": @plan,
+              "run_data[name]": @run,
+              "result_set_data[name]": example.metadata[:description],
+              "result_data[message]": "123123123123",
+              "result_data[status]": "#{result.first}"}
+    request.set_form_data(params)
+    params.merge!({"result_set_data[run_id]": @run_id}) unless @run_id.nil?
+    @run_id = JSON.parse(@http.request(request).body)['run_id']
   end
 
   def get_result(example)
@@ -42,29 +41,48 @@ class Palladium
     result
   end
 end
-#
-#
-# product = "Test1"
-# plan = "v.3"
-# run = File.basename(__FILE__, '_spec.rb')
-# auth = {host: '0.0.0.0', port: '9292', email: 'flamine@list.ru', password: '123123123'}
-# palladium = Palladium.new({:product => product,
-#                            :plan => plan,
-#                            :run => run,
-#                            :auth => auth})
-#
-# describe 'Tests' do
-#   it '1*1' do true end
-#   it '1*2' do expect(true).to eq(false) end
-#   it '1*3' do true end
-#   it '1*4' do  expect(true).to eq(false) end
-#   it '1*5' do true end
-#   it '1*6' do  expect(true).to eq(false) end
-#   it '1*7' do true end
-#   it '1*8' do  expect(true).to eq(false) end
-#   it '1*9' do true end
-#   after :each do |example|
-#     palladium.set_result(example)
-#   end
-# end
-#
+
+
+product = "Test1"
+plan = "v.3"
+run = File.basename(__FILE__, '_spec.rb')
+auth = {host: '0.0.0.0', port: '9292', token: StaticData::TOKEN}
+palladium = Palladium.new({:product => product,
+                           :plan => plan,
+                           :run => run,
+                           :auth => auth})
+112.times do
+  describe 'Tests' do
+    it '1*1' do
+      true
+    end
+    it '1*2' do
+      expect(true).to eq(false)
+    end
+    it '1*3' do
+      true
+    end
+    it '1*4' do
+      expect(true).to eq(false)
+    end
+    it '1*5' do
+      true
+    end
+    it '1*6' do
+      expect(true).to eq(false)
+    end
+    it '1*7' do
+      true
+    end
+    it '1*8' do
+      expect(true).to eq(false)
+    end
+    it '1*9' do
+      true
+    end
+    after :each do |example|
+      palladium.set_result(example)
+    end
+  end
+end
+
