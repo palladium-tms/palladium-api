879ee0215c1ca798a84fac0c19afdf5282bfe45c
Dmitriy Rotatyy <flamine@list.ru>
2018-11-14 15:35:55 +0300

    Fix wrong puma config. It will change only tests using

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,5 @@
 root = Dir.getwd.to_s
 bind "unix://#{root}/tmp/puma/socket/puma.sock"
-# bind 'tcp://0.0.0.0:80'
+bind 'tcp://0.0.0.0:80'
 pidfile "#{root}/tmp/puma/pid/puma.pid"
 stdout_redirect "#{root}/tmp/puma/log/stdout", "#{root}/tmp/puma/log/stderr", true
