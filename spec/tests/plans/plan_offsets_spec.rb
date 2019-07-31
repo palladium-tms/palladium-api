3f5b3344bfa0ee14cc583f0e5da30c84b37e6fab
Dmitriy Rotatyy <flamine@list.ru>
2019-07-31 16:13:26 +0300

    Add tests for check getting plans by certain plan by id

--- spec/tests/plans/plan_offsets_spec.rb
+++ spec/tests/plans/plan_offsets_spec.rb
@@ -54,4 +54,39 @@ describe 'Plan Smoke' do
       expect(plans_stack.plans.count).to eq(3)
     end
   end
+
+  describe 'Get plans before plan_with id' do
+    it 'check with first plan_id' do
+      6.times { @user.create_new_plan(product_name: @product.name) }
+      plan = @user.create_new_plan(product_name: @product.name)
+      plans_pack = @user.get_plans(product_id: @product.id, plan_id: plan.id).plans
+      expect(plans_pack.count).to eq(3)
+      expect(plans_pack.first.id).to eq(plan.id)
+    end
+
+    it 'check with 7th plan_id' do
+      3.times { @user.create_new_plan(product_name: @product.name) }
+      plan = @user.create_new_plan(product_name: @product.name)
+      6.times { @user.create_new_plan(product_name: @product.name) }
+      plans_pack = @user.get_plans(product_id: @product.id, plan_id: plan.id)
+      expect(plans_pack.plans.count).to eq(7)
+      expect(plans_pack.plans.last.id).to eq(plan.id)
+    end
+
+    it 'check with wrong plan_id' do
+      9.times { @user.create_new_plan(product_name: @product.name) }
+      plan = @user.create_new_plan(product_name: @product.name)
+      plans_pack = @user.get_plans(product_id: @product.id, plan_id: 100_500)
+      expect(plans_pack.plans.count).to eq(3)
+      expect(plans_pack.plans.first.id).to eq(plan.id)
+    end
+
+    it 'check with incorrect plan_id type' do
+      9.times { @user.create_new_plan(product_name: @product.name) }
+      plan = @user.create_new_plan(product_name: @product.name)
+      plans_pack = @user.get_plans(product_id: @product.id, plan_id: 'string')
+      expect(plans_pack.plans.count).to eq(3)
+      expect(plans_pack.plans.first.id).to eq(plan.id)
+    end
+  end
 end
