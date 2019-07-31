ba1776243216826e7674ead50c3dc6fe8def60cd
Dmitriy Rotatyy <flamine@list.ru>
2019-07-31 16:11:46 +0300

    Fix error: all plans will be returned if plan_id is string

--- database/models/Product.rb
+++ database/models/Product.rb
@@ -59,7 +59,7 @@ class Product < Sequel::Model
   end
 
   def self.get_plans(*args)
-    args.first['offset'] = 0 unless args.first['offset']
+    args.first['offset'] = args.first['offset'].to_i
     product = if args.first['product_id']
                 Product[id: args.first['product_id']]
               elsif args.first['product_name']
@@ -67,7 +67,7 @@ class Product < Sequel::Model
               end
     begin
       plans = Plan.where(product_id: product.id).order(Sequel.desc(:id))
-      return [plans.limit(3, args.first['offset']).all, []] unless args.first['plan_id']
+      return [plans.limit(3, args.first['offset']).all, []] unless args.first['plan_id'].to_i != 0
 
       plans_by_id = plans.where(Sequel.lit('id >= ?', args.first['plan_id'].to_i)).all
       return [plans.limit(3, args.first['offset']).all, []] if plans_by_id.count <= 3
