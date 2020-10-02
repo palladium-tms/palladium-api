6050ce373636779143be7d53b67cd887db016b54
Dmitriy Rotatyy <kvazilife@gmail.com>
2020-10-02 17:09:02 +0300

    Fix/sutie deleting (#67)
    * fix suite deleting from plan and add test for it
    
    * Add note to changelog

--- server.rb
+++ server.rb
@@ -386,6 +386,7 @@ class Api < Sinatra::Base
           Plan.add_all_cases(plan)
         end
         suite = plan.remove_suite( Suite[id: params['suite_data']['id']])
+        suite.update(deleted: true)
         plan = Plan.remove_cases_by_suite(plan, suite)
       rescue StandardError => e
         errors = e
