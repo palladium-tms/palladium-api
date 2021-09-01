ffa462837d000ed5114daa4e03ea01572f7700e1
Pavel Lobashov <shockwavenn@gmail.com>
2021-09-01 12:45:08 +0300

    Fix rubocop `Lint/ParenthesesAsGroupedExpression` cop

--- spec/tests/api_token/token_smoke_spec.rb
+++ spec/tests/api_token/token_smoke_spec.rb
@@ -25,7 +25,7 @@ describe 'Auth Smoke' do
   end
 
   describe 'use api token' do
-    let (:rand_word) { Faker::TvShows::Buffy.celebrity }
+    let(:rand_word) { Faker::TvShows::Buffy.celebrity }
     it 'Use api token for result_new method' do
       token = @user.create_new_api_token
       @user_2 = AccountFunctions.create_and_parse
