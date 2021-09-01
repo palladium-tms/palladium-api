9a7c2ccfe721439900a0cd77338df2c126e4ca5c
Pavel Lobashov <shockwavenn@gmail.com>
2021-09-01 14:32:35 +0300

    Fix rubocop `Style/NegatedIfElseCondition` cop

--- database/models/Product.rb
+++ database/models/Product.rb
@@ -110,7 +110,11 @@ class Product < Sequel::Model
   end
 
   def self.get_cases_count(suites, plan)
-    if !plan.cases.empty?
+    if plan.cases.empty?
+      Case.where(suite_id: suites.map(&:id)).group_and_count(:suite_id).map(&:values).group_by do |e|
+        e[:suite_id]
+      end
+    else
       case_ids = plan.cases.map(&:id)
       Case.where(id: case_ids)
           .where(suite_id: suites.map(&:id))
@@ -118,10 +122,6 @@ class Product < Sequel::Model
         e[:suite_id]
       end
 
-    else
-      Case.where(suite_id: suites.map(&:id)).group_and_count(:suite_id).map(&:values).group_by do |e|
-        e[:suite_id]
-      end
     end
   end
 end
