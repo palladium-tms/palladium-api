f0f0da624c411e34cd7046f207ec5da61b6a2f28
Pavel Lobashov <shockwavenn@gmail.com>
2021-09-02 00:28:40 +0300

    Allow development without JWT

--- server.rb
+++ server.rb
@@ -590,9 +590,7 @@ class Public < Sinatra::Base
   post '/registration' do
     cross_origin
     valid_status = Invite.check_link_validation(user_data['invite'])
-    # ENV['RACK_ENV'] == 'development' for debug
-    #
-    if User.all.empty? || (ENV['RACK_ENV'] == 'test' && params['invite'].nil?)
+    if User.all.empty? || (ENV['RACK_ENV'] != 'production' && params['invite'].nil?)
       valid_status[0] = true
       valid_status[1] = []
     end
